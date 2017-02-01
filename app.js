7b0df69b662e120a2fb62193b930cc765162838d
FURKAN ÖZGÜN <ozgunfurkan2626@gmail.com>
2017-02-01 23:11:43 +0200

    All updates done

--- app.js
+++ app.js
@@ -2,45 +2,22 @@ var http = require('http'),
     express = require('express'),
     port = process.env.PORT || 3000;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< .merge_file_a07372
 var my_page = require('./routes/my-page');
-=======
 var about_us = require('./routes/about-us');
->>>>>>> .merge_file_a09740
-=======
-var polls =require('./routes/polls')
->>>>>>> polls-ejs-page-routes
-=======
 var index = require('./routes/index');
->>>>>>> index-ejs-page-routes
+var polls = require('./routes/polls');
+
 
 var app = express();
-
 app.set('view engine','ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< .merge_file_a07372
 app.use('/my_page',my_page);
-
-=======
 app.use('/about_us',about_us);
->>>>>>> .merge_file_a09740
-
-=======
-app.use('/polls',polls);
->>>>>>> polls-ejs-page-routes
-
-app.get('/', function (request, response) {
-    response.render("pages/index");
-});
-=======
 app.use('/',index);
+app.use('/polls',polls);
 
->>>>>>> index-ejs-page-routes
+
 var server = http.createServer(app);
 
 server.listen(port, function(){
