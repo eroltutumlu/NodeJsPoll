9ba84ee78bf217c794710568c7e8bc3f49a74bd3
FURKAN ÖZGÜN <ozgunfurkan2626@gmail.com>
2017-02-01 14:47:19 +0200

    app.js modified and index.js created. We can access index.ejs by using routes

--- app.js
+++ app.js
@@ -2,14 +2,13 @@ var http = require('http'),
     express = require('express'),
     port = process.env.PORT || 3000;
 
+var index = require('./routes/index');
+
 var app = express();
 app.set('view engine','ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
-
-app.get('/', function (request, response) {
-    response.render("pages/index");
-});
+app.use('/',index);
 
 var server = http.createServer(app);
 
