9e105b5eeae78af731232a6d6ed5f695bede793a
FURKAN ÖZGÜN <ozgunfurkan2626@gmail.com>
2017-02-01 22:48:33 +0200

    my-page.ejs and about-us merged

--- app.js
+++ app.js
@@ -2,10 +2,23 @@ var http = require('http'),
     express = require('express'),
     port = process.env.PORT || 3000;
 
+<<<<<<< .merge_file_a07372
+var my_page = require('./routes/my-page');
+=======
+var about_us = require('./routes/about-us');
+>>>>>>> .merge_file_a09740
+
 var app = express();
 app.set('view engine','ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(express.static(__dirname + "/views"));
+<<<<<<< .merge_file_a07372
+app.use('/my_page',my_page);
+
+=======
+app.use('/about_us',about_us);
+>>>>>>> .merge_file_a09740
+
 
 app.get('/', function (request, response) {
     response.render("pages/index");
---
+++ routes/about-us.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('../views/pages/about-us', { title: 'Express' }); //render çağırmak analmında kullanılır
+});
+
+module.exports = router; //modüle dışarda erişim sağlayabilmek için kodu export ediyoruz
